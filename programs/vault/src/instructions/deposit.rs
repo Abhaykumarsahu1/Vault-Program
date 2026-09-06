a52a8a01b5fc84b45497a1512a21d1dd147c7ec0
Abhay Kumar Sahu <aks.abhaykumarsahu@gmail.com>
2026-09-06 09:37:52 +0000

    deposit feature implemented for vault program

--- programs/vault/src/instructions/deposit.rs
+++ programs/vault/src/instructions/deposit.rs
@@ -8,3 +8,38 @@ use crate::{
     state::VaultState,
 }
 
+#[derive(Accounts)]
+pub struct Deposit<'info>{
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [VAULT_SEED, user.key().as_ref()],
+        bump,
+    )]
+    pub vault: SystemAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+
+    //vaultstae we do not require here
+}
+
+pub fn deposit(ctx: Context<Deposit>, amount:u64)->Result<()>{
+
+    let cpi_accounts = Transfer{
+        from: ctx.accounts.user.to_account_info(),
+        to: ctx.accounts.vault.to_account_info(),
+    };
+
+    let cpi_ctx = CpiContext::new(
+        ctx.accounts.system_program.key(),
+        cpi_accounts,
+    )
+
+    Transfer(cpi_ctx, amount)?; //if the transfer fails then error will propogate 
+
+    Ok(())
+
+}
+
